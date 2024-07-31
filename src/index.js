25c6f8be9dc7a506064ca492ab09e3c12d7bf0ee
KRYPTONITE\k.zamotin <k.zamotin@kryptonite.ru>
2024-07-31 13:53:25 +0300

    feat: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+
+  if (n<0) {
+    n *= -1
+  }
+  str = n + ""
+  result = ""
+  for (let i = 0; i < str.length; i++) {
+    result = str.charAt(i) + result;
+  }
+  return result;
 }
